9baaadf61cfd34218717acb43d2c289c099081e2
Michael Svintsitskii <starenginegames@gmail.com>
2021-10-05 17:36:25 +1000

    Added comments

--- WinForms_Monday/Form1.cs
+++ WinForms_Monday/Form1.cs
@@ -24,11 +24,20 @@ namespace WinForms_Monday
         private void Form1_Load(object sender, EventArgs e)
         {
             listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
         }
+
+        /*
+            1) Запустить через CTRL+F5 два экземпляра приложения
+            2) В одном выбрать Сервер и нажать подключиться 
+            3) В другом выбрать Клиент и нажать подключиться
+            4) В экземпляре с выбранным Сервер нажать на квадратную кнопку. Это событие перекрасит её, 
+                добавит текст и отправит в экземпляр с Клиентом, что вызовет изменение внешнего вида кнопки и в нем.
+        */
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Подключение если выбран сервер
             if(radioButton1.Checked)
             {
                 listenSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000)); //ip адрес сервера
@@ -38,14 +47,13 @@ namespace WinForms_Monday
 
                 client = listenSocket.Accept();
             }
+            //Если выбран клиент
             else
             {
                 textBox1.Text = "Ходит сервер";
                 try
                 {
                     listenSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000));
-
-                    
                 }
                 catch (Exception ex)
                 {
@@ -56,8 +64,9 @@ namespace WinForms_Monday
                 int bytes = 0;
                 byte[] d = new byte[256]; // буфер для получаемых данных
 
-                bytes = listenSocket.Receive(d);
+                bytes = listenSocket.Receive(d); //ожидание получения данных
 
+                // полученные данные
                 string ass = Encoding.Unicode.GetString(d, 0, bytes);
                 if(ass == "1")
                 {
@@ -84,6 +93,7 @@ namespace WinForms_Monday
             }
         }
 
+
         public void work(String num, Socket s)
         {
             byte[] data = Encoding.Unicode.GetBytes(num);
@@ -93,32 +103,19 @@ namespace WinForms_Monday
             s.Receive(getData);
 
         }
+
+        //Нажатие на квадратную кнопку
         private void button2_Click(object sender, EventArgs e)
         {
             if (radioButton1.Checked)
             {
                 button2.BackColor = System.Drawing.Color.Red;
                 button2.Text = "O";
+                button2.Enabled = false;
 
                 work("1", client);
                 textBox1.Text = "Ходит клиент";
-
-
-                button2.Enabled = false;
             }
-            else
-            {
-                button2.BackColor = System.Drawing.Color.LightGreen;
-                button2.Text = "X";
-
-                work("11", client);
-                textBox1.Text = "Ходит Сервер";
-                
-
-                button2.Enabled = false;
-            }
-        }
-
-        
+        } 
     }
 }
